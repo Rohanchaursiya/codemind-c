11edc4e399c73a57b8a265bab3af58ac218bd473
Rohan Kumar Chaursiya <97392604+Rohanchaursiya@users.noreply.github.com>
2022-03-06 13:44:56 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n][n],i,j,pd=0,sd=0;     for(i=0;i<n;i++)     {         for(j=0;j<n;j++)         {             scanf("%d",&a[i][j]);
             if(i==j)
             {
              pd+=a[i][j];
             }
             if(i+j==n-1)
             {
                sd+=a[i][j];
             }
            }
        }
        printf("Principal Diagonal:%d
    ",pd);
        printf("Secondary Diagonal:%d",sd);
        return 0;
    }

---
+++ Calculate_sums_of_diagonals_of_a_matrix.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n][n],i,j,pd=0,sd=0;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&a[i][j]);
+        
+         if(i==j)
+         {
+          pd+=a[i][j];
+         }
+         if(i+j==n-1)
+         {
+            sd+=a[i][j];
+         }
+        }
+    }
+    printf("Principal Diagonal:%d
+",pd);
+    printf("Secondary Diagonal:%d",sd);
+    return 0;
+}
