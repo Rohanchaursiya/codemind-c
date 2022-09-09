2a083991c3a504d8abeea67a032a652e57b24567
Rohan Kumar Chaursiya <97392604+Rohanchaursiya@users.noreply.github.com>
2022-09-09 18:02:43 +0530

    #include<stdio.h> int main(){     int m,n;     scanf("%d%d",&m,&n);     int a[m][n];     int i,j;     for(i=0;i<m;i++){         for(j=0;j<n;j++){             scanf("%d",&a[i][j]);         }     }     int e=0,o=0;     for(i=0;i<m;i++){         for(j=0;j<n;j++){                 if(a[i][j]%2==0){                     e+=a[i][j];                 }                 else{                     o+=a[i][j];                 }         }     }     printf("%d %d",e,o);     return 0; }

---
+++ Sum_of_even_and__odd_elements_in_a_matrix.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    int m,n;
+    scanf("%d%d",&m,&n);
+    int a[m][n];
+    int i,j;
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    int e=0,o=0;
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+                if(a[i][j]%2==0){
+                    e+=a[i][j];
+                }
+                else{
+                    o+=a[i][j];
+                }
+        }
+    }
+    printf("%d %d",e,o);
+    return 0;
+}
