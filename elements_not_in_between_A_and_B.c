a779aeee6a0e6c351ced0173bb6f83d099cd4ba2
Rohan Kumar Chaursiya <97392604+Rohanchaursiya@users.noreply.github.com>
2022-09-19 00:28:45 +0530

    #include<stdio.h> int main(){     int n;     scanf("%d",&n);     int arr[100];     for(int i=0;i<n;i++){         scanf("%d",&arr[i]);     }     int a,b;     scanf("%d%d",&a,&b);     int c=0;     for(int i=0;i<n;i++){         if(arr[i]<a || arr[i]>b){             printf("%d ",arr[i]);             c+=1;         }     }     if(c==0){         printf("-1");     } }

--- elements_not_in_between_A_and_B.c
+++ elements_not_in_between_A_and_B.c
@@ -1,31 +1,21 @@
 #include<stdio.h>
-int main()
-{
+int main(){
     int n;
     scanf("%d",&n);
-    int arr[n],i,a,b,count=0;
-    for(i=0;i<n;i++)
-    {
+    int arr[100];
+    for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
+    int a,b;
     scanf("%d%d",&a,&b);
-    if(a>b)
-    {
-        count=a;
-        a=b;
-        b=count;
-    }
-    count=-1;
-    for(i=0;i<n;i++)
-    {
-        if(arr[i]<a||arr[i]>b)
-        {
-            count=arr[i];
+    int c=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]<a || arr[i]>b){
             printf("%d ",arr[i]);
+            c+=1;
         }
     }
-    if(count==-1)
-    {
-        printf("%d",count);
+    if(c==0){
+        printf("-1");
     }
 }
